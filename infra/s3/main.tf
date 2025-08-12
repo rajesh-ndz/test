325f5199cdbde44d01766aa8a1cd7c543503a908
rajesh <rajesh.r@ndimensionz.com>
2025-08-12 10:31:33 +0530

    Change bucket name

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -1,7 +1,7 @@
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "artifact" {
-  bucket = "idlms-${var.environment}-built-artifact-${data.aws_caller_identity.current.account_id}"
+  bucket = "test-s3-idlmreplatforming-artifact"
 
   tags = {
     Name        = "IDLMS ${var.environment} Artifact Bucket"
