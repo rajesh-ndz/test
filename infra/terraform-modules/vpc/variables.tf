937b2c90f0f7c97b8811483f99c3b866df4c8292
rajesh <rajesh.r@ndimensionz.com>
2025-08-14 17:52:49 +0530

    old revert

--- infra/terraform-modules/vpc/variables.tf
+++ infra/terraform-modules/vpc/variables.tf
@@ -1,88 +1,3 @@
-# variable "environment" {
-#   description = "Environment for deployment"
-#   type        = string
-# }
-
-# variable "common_tags" {
-#   description = "Common tags for resources"
-#   type        = map(string)
-# }
-
-# variable "region" {
-#   description = "AWS region"
-#   type        = string
-# }
-
-# variable "instance_tenancy" {
-#   description = "VPC instance tenancy"
-#   type        = string
-# }
-
-# variable "enable_dns_support" {
-#   description = "Enable DNS support for VPC"
-#   type        = bool
-# }
-
-# variable "enable_dns_hostnames" {
-#   description = "Enable DNS hostnames for VPC"
-#   type        = bool
-# }
-
-# variable "vpc_name" {
-#   description = "Name of the VPC"
-#   type        = string
-# }
-
-# variable "vpc_cidr" {
-#   description = "CIDR block for the VPC"
-#   type        = string
-# }
-
-# variable "internet_gateway_name" {
-#   description = "Name of the Internet Gateway"
-#   type        = string
-# }
-
-# variable "total_nat_gateway_required" {
-#   description = "Number of NAT Gateways required"
-#   type        = number
-# }
-
-# variable "eip_for_nat_gateway_name" {
-#   description = "Name prefix for NAT Gateway Elastic IPs"
-#   type        = string
-# }
-
-# variable "nat_gateway_name" {
-#   description = "Name prefix for NAT Gateways"
-#   type        = string
-# }
-
-
-# ###Private App Subnets###
-# variable "private_subnets" {
-#   type = object({
-#     routes              = list(any)
-#     cidrs_blocks        = list(string)
-#     subnets_name_prefix = string
-#     route_table_name    = string
-#   })
-# }
-
-
-# ###Public Subnets###
-# variable "public_subnets" {
-#   type = object({
-#     routes                  = list(any)
-#     cidrs_blocks            = list(string)
-#     subnets_name_prefix     = string
-#     map_public_ip_on_launch = bool
-#     route_table_name        = string
-#   })
-# }
-
-
-
 variable "environment" {
   description = "Environment for deployment"
   type        = string
@@ -143,7 +58,8 @@ variable "nat_gateway_name" {
   type        = string
 }
 
-### Private App Subnets ###
+
+###Private App Subnets###
 variable "private_subnets" {
   type = object({
     routes              = list(any)
@@ -153,7 +69,8 @@ variable "private_subnets" {
   })
 }
 
-### Public Subnets ###
+
+###Public Subnets###
 variable "public_subnets" {
   type = object({
     routes                  = list(any)
@@ -164,9 +81,3 @@ variable "public_subnets" {
   })
 }
 
-# OPTIONAL: pin to specific AZs; if left empty we auto-detect AZs in the region
-variable "azs" {
-  description = "List of AZs to spread subnets across (leave empty to auto-detect)"
-  type        = list(string)
-  default     = []
-}
