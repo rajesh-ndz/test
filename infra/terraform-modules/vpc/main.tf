78e99dd4c975dbb99f1fcefa0a07d33983224849
rajesh <rajesh.r@ndimensionz.com>
2025-08-14 17:21:33 +0530

    checking the azs

--- infra/terraform-modules/vpc/main.tf
+++ infra/terraform-modules/vpc/main.tf
@@ -1,3 +1,99 @@
+# resource "aws_vpc" "vpc" {
+#   cidr_block           = var.vpc_cidr
+#   instance_tenancy     = var.instance_tenancy
+#   enable_dns_support   = var.enable_dns_support
+#   enable_dns_hostnames = var.enable_dns_hostnames
+
+#   tags = merge(var.common_tags, { Name = var.vpc_name })
+# }
+
+# # Internet Gateway for Public Subnets
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.vpc.id
+
+#   tags = merge(var.common_tags, { Name = var.internet_gateway_name })
+# }
+
+# # Elastic IP for NAT Gateway
+# resource "aws_eip" "nat_gateway" {}
+
+# # NAT Gateway for Private Subnets
+# resource "aws_nat_gateway" "nat" {
+#   allocation_id = aws_eip.nat_gateway.id
+#   subnet_id     = aws_subnet.public_subnets[0].id # NAT placed in first public subnet
+
+#   tags = merge(var.common_tags, { Name = var.nat_gateway_name })
+# }
+
+# resource "aws_subnet" "public_subnets" {
+#   count                   = length(var.public_subnets["cidrs_blocks"])
+#   vpc_id                  = aws_vpc.vpc.id
+#   cidr_block              = element(var.public_subnets["cidrs_blocks"], count.index)
+#   map_public_ip_on_launch = true
+#   tags                    = merge(var.common_tags, { Name = "Public Subnet ${count.index + 1}" })
+# }
+
+# resource "aws_subnet" "private_subnets" {
+#   count      = length(var.private_subnets["cidrs_blocks"])
+#   vpc_id     = aws_vpc.vpc.id
+#   cidr_block = element(var.private_subnets["cidrs_blocks"], count.index)
+#   tags       = merge(var.common_tags, { Name = "Private Subnet ${count.index + 1}" })
+# }
+
+
+
+
+# # Public Route Table (Routes traffic to the Internet Gateway)
+# resource "aws_route_table" "public_rt" {
+#   vpc_id = aws_vpc.vpc.id
+#   tags   = merge(var.common_tags, { Name = "Public Route Table" })
+# }
+
+# resource "aws_route" "public_route" {
+#   route_table_id         = aws_route_table.public_rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.igw.id
+# }
+
+# # Private Route Table (Routes traffic via NAT Gateway)
+# resource "aws_route_table" "private_rt" {
+#   vpc_id = aws_vpc.vpc.id
+#   tags   = merge(var.common_tags, { Name = "Private Route Table" })
+# }
+
+# resource "aws_route" "private_route" {
+#   route_table_id         = aws_route_table.private_rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id         = aws_nat_gateway.nat.id
+# }
+
+# # Associate Public Subnets with Public Route Table
+# resource "aws_route_table_association" "public_subnet_assoc" {
+#   count          = length(aws_subnet.public_subnets)
+#   subnet_id      = aws_subnet.public_subnets[count.index].id
+#   route_table_id = aws_route_table.public_rt.id
+# }
+
+# # Associate Private Subnets with Private Route Table
+# resource "aws_route_table_association" "private_subnet_assoc" {
+#   count          = length(aws_subnet.private_subnets)
+#   subnet_id      = aws_subnet.private_subnets[count.index].id
+#   route_table_id = aws_route_table.private_rt.id
+# }
+
+# --- New: discover AZs and set locals for AZ spreading ---
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  # Use user-provided AZs if given, otherwise take all AZs in the region
+  azs = length(var.azs) > 0 ? var.azs : data.aws_availability_zones.available.names
+
+  # Also used by outputs to pick first 3 AZs for VPC Link
+  vpc_link_supported_azs = slice(local.azs, 0, min(3, length(local.azs)))
+}
+
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   instance_tenancy     = var.instance_tenancy
@@ -10,8 +106,7 @@ resource "aws_vpc" "vpc" {
 # Internet Gateway for Public Subnets
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
-
-  tags = merge(var.common_tags, { Name = var.internet_gateway_name })
+  tags   = merge(var.common_tags, { Name = var.internet_gateway_name })
 }
 
 # Elastic IP for NAT Gateway
@@ -21,27 +116,27 @@ resource "aws_eip" "nat_gateway" {}
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_gateway.id
   subnet_id     = aws_subnet.public_subnets[0].id # NAT placed in first public subnet
-
-  tags = merge(var.common_tags, { Name = var.nat_gateway_name })
+  tags          = merge(var.common_tags, { Name = var.nat_gateway_name })
 }
 
+# -------------- CHANGED: assign AZ per public subnet --------------
 resource "aws_subnet" "public_subnets" {
   count                   = length(var.public_subnets["cidrs_blocks"])
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = element(var.public_subnets["cidrs_blocks"], count.index)
-  map_public_ip_on_launch = true
+  availability_zone       = local.azs[count.index % length(local.azs)]
+  map_public_ip_on_launch = var.public_subnets["map_public_ip_on_launch"]
   tags                    = merge(var.common_tags, { Name = "Public Subnet ${count.index + 1}" })
 }
 
+# -------------- CHANGED: assign AZ per private subnet --------------
 resource "aws_subnet" "private_subnets" {
-  count      = length(var.private_subnets["cidrs_blocks"])
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = element(var.private_subnets["cidrs_blocks"], count.index)
-  tags       = merge(var.common_tags, { Name = "Private Subnet ${count.index + 1}" })
+  count             = length(var.private_subnets["cidrs_blocks"])
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = element(var.private_subnets["cidrs_blocks"], count.index)
+  availability_zone = local.azs[count.index % length(local.azs)]
+  tags              = merge(var.common_tags, { Name = "Private Subnet ${count.index + 1}" })
 }
-
-
-
 
 # Public Route Table (Routes traffic to the Internet Gateway)
 resource "aws_route_table" "public_rt" {
--- infra/terraform-modules/vpc/outputs.tf
+++ infra/terraform-modules/vpc/outputs.tf
@@ -1,12 +1,66 @@
+# # --- VPC Outputs ---
+
+# # VPC ID
+# output "vpc_id" {
+#   description = "VPC ID"
+#   value       = aws_vpc.vpc.id
+# }
+
+# # Default Security Group ID
+# output "default_security_group_id" {
+#   description = "The default security group ID of the VPC"
+#   value       = aws_vpc.vpc.default_security_group_id
+# }
+
+# # --- Public Subnet Outputs ---
+
+# # Public Subnet IDs
+# output "public_subnets_ids" {
+#   description = "IDs of the public subnets"
+#   value       = aws_subnet.public_subnets[*].id
+# }
+
+# # Public Subnet CIDRs
+# output "public_subnets_cidrs" {
+#   description = "CIDR blocks of the public subnets"
+#   value       = aws_subnet.public_subnets[*].cidr_block
+# }
+
+# # --- Private Subnet Outputs ---
+
+# # Private Subnet IDs
+# output "private_subnets_ids" {
+#   description = "IDs of the private subnets"
+#   value       = aws_subnet.private_subnets[*].id
+# }
+
+# # Private Subnet CIDRs
+# output "private_subnets_cidrs" {
+#   description = "CIDR blocks of the private subnets"
+#   value       = aws_subnet.private_subnets[*].cidr_block
+# }
+
+# # Private Subnets in AZs supported by API Gateway VPC Link (us-east-1a/b/c)
+# output "vpc_link_subnet_ids" {
+#   description = "Private subnet IDs in supported AZs for API Gateway VPC Link"
+#   value = [
+#     for s in aws_subnet.private_subnets :
+#     s.id if contains(["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"], s.availability_zone)
+#   ]
+# }
+
+# output "private_subnets_azs" {
+#   description = "AZs of the private subnets"
+#   value       = aws_subnet.private_subnets[*].availability_zone
+# }
+
 # --- VPC Outputs ---
 
-# VPC ID
 output "vpc_id" {
   description = "VPC ID"
   value       = aws_vpc.vpc.id
 }
 
-# Default Security Group ID
 output "default_security_group_id" {
   description = "The default security group ID of the VPC"
   value       = aws_vpc.vpc.default_security_group_id
@@ -14,39 +68,32 @@ output "default_security_group_id" {
 
 # --- Public Subnet Outputs ---
 
-# Public Subnet IDs
 output "public_subnets_ids" {
   description = "IDs of the public subnets"
   value       = aws_subnet.public_subnets[*].id
 }
 
-# Public Subnet CIDRs
 output "public_subnets_cidrs" {
   description = "CIDR blocks of the public subnets"
   value       = aws_subnet.public_subnets[*].cidr_block
 }
 
+# (optional but useful)
+output "public_subnets_azs" {
+  description = "AZs of the public subnets"
+  value       = aws_subnet.public_subnets[*].availability_zone
+}
+
 # --- Private Subnet Outputs ---
 
-# Private Subnet IDs
 output "private_subnets_ids" {
   description = "IDs of the private subnets"
   value       = aws_subnet.private_subnets[*].id
 }
 
-# Private Subnet CIDRs
 output "private_subnets_cidrs" {
   description = "CIDR blocks of the private subnets"
   value       = aws_subnet.private_subnets[*].cidr_block
-}
-
-# Private Subnets in AZs supported by API Gateway VPC Link (us-east-1a/b/c)
-output "vpc_link_subnet_ids" {
-  description = "Private subnet IDs in supported AZs for API Gateway VPC Link"
-  value = [
-    for s in aws_subnet.private_subnets :
-    s.id if contains(["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"], s.availability_zone)
-  ]
 }
 
 output "private_subnets_azs" {
@@ -54,3 +101,11 @@ output "private_subnets_azs" {
   value       = aws_subnet.private_subnets[*].availability_zone
 }
 
+# --- CHANGED: Region-agnostic VPC Link subnet selection (first 3 AZs in region) ---
+output "vpc_link_subnet_ids" {
+  description = "Private subnet IDs in selected AZs for API Gateway VPC Link"
+  value = [
+    for s in aws_subnet.private_subnets :
+    s.id if contains(local.vpc_link_supported_azs, s.availability_zone)
+  ]
+}
